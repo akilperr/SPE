181fe5e9e03cb22ead51146b836c7ef4d5f0dcd5
carlosvelazquezz <cavear2003@gmail.com>
2025-05-11 21:02:54 +0200

    First step: task and job validation

--- lib/spe.ex
+++ lib/spe.ex
@@ -12,7 +12,71 @@ defmodule Spe do
       :world
 
   """
-  def hello do
-    :world
+
+  defmodule TaskDef do
+  @moduledoc """
+  This module manages the tasks validation (synthactically correct)
+  """
+
+    def normalize_task(task) do # Verifies correct fields and completes omitted ones
+      if is_map(task) do
+        name = Map.get(task, "name")
+        exec = Map.get(task, "exec")
+        enables = Map.get(task, "enables", [])
+        timeout = Map.get(task, "timeout", :infinity)
+
+        if is_binary(name) and is_function(exec, 1) and valid_timeout?(timeout) and is_list(enables) do # For the moment, we only verify that enables is a list
+          %{
+            "name" => name,
+            "exec" => exec,
+            "enables" => enables,
+            "timeout" => timeout
+          }
+        else
+          {:error, :invalid_task}
+        end
+      else
+        {:error, :invalid_task}
+      end
+    end
+
+    defp valid_timeout?(:infinity), do: true # Valid timeout: infinity or a positive number
+    defp valid_timeout?(t) when is_integer(t) and t >= 0, do: true
+    defp valid_timeout?(_), do: false
+    def valid_enables?(list, defined_names) when is_list(list) do # Valid enables list: all task names have to be in the task list, used when all job tasks defined
+      Enum.all?(list, fn name -> is_binary(name) and name in defined_names end)
+    end
+    def valid_enables?(_, _), do: false
   end
+
+  defmodule JobValidator do
+  @moduledoc """
+  This module manages the jobs validation (synthactically correct)
+  """
+
+    alias Spe.TaskDef
+
+    def validate_job(job_description) do
+      tasks = Map.get(job_description, "tasks", [])
+
+      validated_tasks = Enum.map(tasks, &TaskDef.normalize_task/1)
+
+      if Enum.any?(validated_tasks, fn t -> t == {:error, :invalid_task} end) do
+        {:error, :invalid_job}
+      else
+        task_names = Enum.map(validated_tasks, fn t -> t["name"] end)
+        validate_task_dependencies(validated_tasks, task_names)
+      end
+    end
+
+    def validate_task_dependencies(tasks, task_names) do
+      if Enum.all?(tasks, fn task -> TaskDef.valid_enables?(task["enables"], task_names) end) do
+        {:ok, tasks}
+      else
+        {:error, :invalid_dependencies}
+      end
+    end
+  end
+
+
 end
--- test/spe_test.exs
+++ test/spe_test.exs
@@ -1,8 +1,70 @@
 defmodule SpeTest do
   use ExUnit.Case
+  alias Spe.TaskDef
+  alias Spe.JobValidator
   doctest Spe
 
-  test "greets the world" do
-    assert Spe.hello() == :world
-  end
+
+    # Tasks validation tests
+    describe "TaskDef.normalize_task/1" do
+      test "Validates correct task" do
+        task = %{"name" => "task1", "exec" => fn _ -> 1 + 2 end}
+        assert TaskDef.normalize_task(task) == %{"name" => "task1", "exec" => task["exec"], "enables" => [], "timeout" => :infinity}
+      end
+
+      test "Verifies validation fails if incorrect name" do
+        task = %{"exec" => fn _ -> 1 + 2 end}
+        assert TaskDef.normalize_task(task) == {:error, :invalid_task}
+      end
+
+      test "Verifies if validation fails with wrong arity" do
+        task = %{"name" => "task1", "exec" => fn -> 1 + 2 end}
+        assert TaskDef.normalize_task(task) == {:error, :invalid_task}
+      end
+
+      test "Verifies if validation fails with incorrect timeout" do
+        task = %{"name" => "task1", "exec" => fn _ -> 1 + 2 end, "timeout" => -5}
+        assert TaskDef.normalize_task(task) == {:error, :invalid_task}
+      end
+
+      test "Verifies validation fails with incorrect enables list" do
+        task = %{"name" => "task1", "exec" => fn _ -> 1 + 2 end, "enables" => "task2"}
+        assert TaskDef.normalize_task(task) == {:error, :invalid_task}
+      end
+    end
+
+    # Jobs validation test
+    describe "JobValidator.validate_job/1" do
+      test "Verifies if validation succeeds with correct job" do
+        job = %{"tasks" => [
+          %{"name" => "task1", "exec" => fn _ -> 1 end},
+          %{"name" => "task2", "exec" => fn %{"task1" => v} -> v + 2 end, "enables" => ["task1"]}
+        ]}
+
+        assert JobValidator.validate_job(job) == {:ok, [
+          %{"name" => "task1", "exec" => job["tasks"] |> hd() |> Map.get("exec"), "enables" => [], "timeout" => :infinity},
+          %{"name" => "task2", "exec" => job["tasks"] |> tl() |> hd() |> Map.get("exec"), "enables" => ["task1"], "timeout" => :infinity}
+        ]}
+
+      end
+
+      test "Verifies if validation fails with an incorrect task" do
+        job = %{"tasks" => [
+          %{"name" => "task1", "exec" => fn _ -> 1 end},
+          %{"exec" => fn _ -> 2 end}  # missing name
+        ]}
+
+        assert JobValidator.validate_job(job) == {:error, :invalid_job}
+      end
+
+      test "Veifies if validation fails with incoherent dependencies" do
+        job = %{"tasks" => [
+          %{"name" => "task1", "exec" => fn _ -> 1 end},
+          %{"name" => "task2", "exec" => fn %{"taskX" => v} -> v + 2 end, "enables" => ["taskX"]}
+        ]}
+
+        assert JobValidator.validate_job(job) == {:error, :invalid_dependencies}
+      end
+    end
+
 end
